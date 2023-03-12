597ae818312ee57d6532bf8e50ff704123e9541c
erlangxk <4946085+erlangxk@users.noreply.github.com>
2023-03-12 22:14:49 +0800

    first db unit test

--- authx.tests/Tests.fs
+++ authx.tests/Tests.fs
@@ -1,11 +1,24 @@
 module Tests
 
 open System
+open Microsoft.Data.Sqlite
 open Xunit
+open authx.MySQLite
 
+let connStr = "Data Source=/Users/simonking/Microsoft/authx/authx.sqlite;"
 
+type MySQLiteTests() as self =
+    let conn = new SqliteConnection(connStr)
+        
+    interface IDisposable with
+        member this.Dispose() = conn.Dispose()
 
+    [<Fact>]
+    member self.``My test``() =
+        let f = getClientById ("first_client")
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+        task {
+            match! f conn with
+            | Ok(client) -> Assert.Equal("first_secret", client.Value.Secret)
+            | Error(err) -> Assert.True(false)
+        }
